39093a30fc58866cdbcca7551191a349cd20595d
Gabby <gabbie10112005@gmail.com>
2024-12-11 09:38:55 -0600

    Initial commit

---
+++ Methods_Miller_Gabriella/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Methods_Miller_Gabriella
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
